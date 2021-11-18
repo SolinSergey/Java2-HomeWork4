f6c9681aa55e245096881377a87e3b94930ea1ed
Sergey <livegrafics@rambler.ru>
2021-11-18 12:54:30 +0300

    Corrected

--- src/main/java/com/homework/homework4/HomeWork4.java
+++ src/main/java/com/homework/homework4/HomeWork4.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class HomeWork4 extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("homework4.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HomeWork4.class.getResource("homework4.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 400, 580);
+        stage.setTitle("Сетевой чат");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/homework/homework4/HomeWork4Controller.java
@@ -0,0 +1,24 @@
+package com.homework.homework4;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class HomeWork4Controller {
+    @FXML
+    private TextArea mainTextArea;
+    @FXML
+    private TextField mainTextField;
+
+    @FXML
+    protected void textFieldEnterPressed(){
+        onButtonClick();
+    }
+    @FXML
+    protected void onButtonClick() {
+        String s = mainTextField.getText();
+        mainTextArea.appendText(s+"\n");
+        mainTextField.setText("");
+    }
+}
